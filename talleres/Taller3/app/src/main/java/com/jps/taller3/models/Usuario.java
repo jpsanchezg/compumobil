6f4efd59e0dbc8d9817654a302ad3a02226ce5f9
juan pablo sanchez <juansanchezg@javeriana.edu.co>
2022-10-31 22:03:15 -0500

    Colocando los atributos de usuario

--- talleres/Taller3/app/src/main/java/com/jps/taller3/models/Usuario.java
+++ talleres/Taller3/app/src/main/java/com/jps/taller3/models/Usuario.java
@@ -1,4 +1,78 @@
 package com.jps.taller3.models;
 
 public class Usuario {
+    private String nombre;
+    private String apellido;
+    private String correo;
+    private String fotodeperfil;
+    private String numerodeidentificacion;
+    private Double latitud;
+    private Double longitud;
+
+
+    public Usuario(String nombre, String apellido, String correo, String fotodeperfil, String numerodeidentificacion, Double latitud, Double longitud) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.correo = correo;
+        this.fotodeperfil = fotodeperfil;
+        this.numerodeidentificacion = numerodeidentificacion;
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getFotodeperfil() {
+        return fotodeperfil;
+    }
+
+    public void setFotodeperfil(String fotodeperfil) {
+        this.fotodeperfil = fotodeperfil;
+    }
+
+    public String getNumerodeidentificacion() {
+        return numerodeidentificacion;
+    }
+
+    public void setNumerodeidentificacion(String numerodeidentificacion) {
+        this.numerodeidentificacion = numerodeidentificacion;
+    }
+
+    public Double getLatitud() {
+        return latitud;
+    }
+
+    public void setLatitud(Double latitud) {
+        this.latitud = latitud;
+    }
+
+    public Double getLongitud() {
+        return longitud;
+    }
+
+    public void setLongitud(Double longitud) {
+        this.longitud = longitud;
+    }
 }
